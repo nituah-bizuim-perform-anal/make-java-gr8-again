81406aba642b8deec4710fe713301130b87ab01a
Gil9 <Gilzm99@gmail.com>
2019-08-27 23:13:52 +0300

    fixed actor test interfaces maps

--- src/main/java/org/test/MapActorTest.java
+++ src/main/java/org/test/MapActorTest.java
@@ -1,24 +1,23 @@
 package org.test;
 
-import org.base.TimedMap;
+import org.base.TimedSizableMap;
 import org.junit.Test;
 import org.single_actor.SingleActorTimedSizableHashMap;
 
 import java.util.concurrent.TimeUnit;
-
 import static junit.framework.TestCase.assertEquals;
 
 public class MapActorTest {
 
     @Test
     public void SingleMapActor_GetSize(){
-        TimedMap.TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
+        TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
         assertEquals(0,map.size());
     }
 
     @Test
     public void SingleMapActor_Insert() throws InterruptedException {
-        TimedMap.TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
+        TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
         map.put("gil", 420, 69, TimeUnit.SECONDS);
         map.put("gil2", 421, 629, TimeUnit.SECONDS);
         map.put("gil3", 4200, 619, TimeUnit.SECONDS);
@@ -29,7 +28,7 @@ public class MapActorTest {
 
     @Test
     public void SingleMapActor_Remove_Key() throws InterruptedException {
-        TimedMap.TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
+        TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
         map.put("gil", 420, 69, TimeUnit.SECONDS);
         Thread.sleep(2000);
         map.remove("gil");
@@ -39,7 +38,7 @@ public class MapActorTest {
 
     @Test
     public void SingleMapActor_Remove_Empty() throws InterruptedException {
-        TimedMap.TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
+        TimedSizableMap<String, Integer> map = new SingleActorTimedSizableHashMap<>();
         map.put("gil", 420, 69, TimeUnit.SECONDS);
         Thread.sleep(2000);
         map.remove("gil");
