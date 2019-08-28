a11ab59e3155b95196bed63d9171001d4894837f
Dean Kevorkian <deankevor@gmail.com>
2019-08-28 20:58:46 +0300

    Attempt at using ConcurrentHashMap with it's atomic methods.

--- src/main/java/org/imperialistic/CapitalisticTimedSizableHashMap.java
+++ src/main/java/org/imperialistic/CapitalisticTimedSizableHashMap.java
@@ -13,14 +13,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class CapitalisticTimedSizableHashMap<K, V> implements TimedSizableMap<K, V> {
     private final ScheduledExecutorService removalService = Executors.newSingleThreadScheduledExecutor();
-    private final ConcurrentHashMap<K, StampedObject<V>> data;
-    private final AtomicLong counter = new AtomicLong(1);
+    private final ConcurrentHashMap<K, V> data;
 
     public CapitalisticTimedSizableHashMap() {
         this(new ConcurrentHashMap<>());
     }
 
-    protected CapitalisticTimedSizableHashMap(Map<K, StampedObject<V>> map) {
+    protected CapitalisticTimedSizableHashMap(Map<K, V> map) {
         this.data = new ConcurrentHashMap<>(map);
     }
 
@@ -31,17 +30,19 @@ public class CapitalisticTimedSizableHashMap<K, V> implements TimedSizableMap<K,
 
     @Override
     public void put(K key, V value, int duration, TimeUnit unit) {
-        this.data.put(k)
+        this.data.put(key, value);
+
+        this.removalService.schedule(() -> this.data.compute(key, (k, v) -> v.equals(value) ? null: v), duration, unit);
     }
 
     @Override
     public Optional<V> get(K key) {
-        return Optional.ofNullable(this.data.get(key).obj);
+        return Optional.ofNullable(this.data.get(key));
     }
 
     @Override
     public Optional<V> remove(K key) {
-        return Optional.empty();
+        return Optional.ofNullable(this.data.remove(key));
     }
 
 
