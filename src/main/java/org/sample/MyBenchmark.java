d8aba9b945e09943168a562aa54b2c39413818b2
Gil9 <Gilzm99@gmail.com>
2019-08-29 20:33:54 +0300

    watch me save you 8 minutes of your life

--- src/main/java/org/sample/MyBenchmark.java
+++ src/main/java/org/sample/MyBenchmark.java
@@ -35,7 +35,6 @@ import org.openjdk.jmh.annotations.Benchmark;
 
 public class MyBenchmark {
 
-    @Benchmark
     public void testMethod() {
         // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
         // Put your benchmark code here.
